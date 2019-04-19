3c5e34b744f0b448cd18b95462dabf9a8bf7e725
Pato05 <lo.gu2005@hotmail.it>
2019-04-19 21:26:30 +0200

    Updated docs, and you can now create new circularProgress elements

--- src/circular-progress.js
+++ src/circular-progress.js
@@ -1,19 +1,21 @@
 "use strict";
-!(function() {
-    for(let el of document.querySelectorAll("div.circularPercent")) {
-        if(el.getAttribute("data-percent")) {
-            if(!isNaN(el.getAttribute("data-percent"))) {
+var circularProgress = {
+    new: function(el, progress, label = false) {
+        if(el.getAttribute("data-percent") || progress) {
+            if(!isNaN(el.getAttribute("data-percent")) || !isNaN(progress)) {
                 if(el.getAttribute("data-percent") <= 100){
                     var text = null;
                     var strokeWidth = el.getAttribute("data-stroke-width") ? el.getAttribute("data-stroke-width") : 10;
                     var stroke = el.getAttribute("data-stroke") ? el.getAttribute("data-stroke") : "#2ecc71";
                     var textColor = el.getAttribute("data-text-color") ? el.getAttribute("data-text-color") : "#2ecc71";
-                    if(el.getAttribute("data-text")) {
+                    if(el.getAttribute("data-text") || label) {
                         text = document.createElementNS("http://www.w3.org/2000/svg", "text");
                     }
+                    if(!el.classList.contains("circularProgress")) el.classList.add("circularProgress");
+                    if(!el.getAttribute("data-percent")) el.setAttribute("data-percent", progress);
                     var box = document.createElementNS("http://www.w3.org/2000/svg","svg");
                     var circle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-                    var percent = el.getAttribute("data-percent");
+                    var percent = progress ? progress : el.getAttribute("data-percent");
                     var offset = (percent != 0) ? 603.2699584960938 - (percent * 603.2699584960938 / 100) : 603.2699584960938;
                     var width = el.getAttribute("data-width") ? el.getAttribute("data-width") : 200;
                     var height = el.getAttribute("data-height") ? el.getAttribute("data-height") : 200;
@@ -31,11 +33,12 @@
                     box.appendChild(circle);
                     el.appendChild(box);
                     if(text) {
+                        text.appendChild(document.createTextNode(el.getAttribute("data-text") ? el.getAttribute("data-text") : label));
                         box.appendChild(text);
-                        text.appendChild(document.createTextNode(el.getAttribute("data-text")));
-                        var posInfo = text.getBoundingClientRect();
-                        text.setAttribute("x", (200 / 2) - (posInfo.width / 2));
-                        text.setAttribute("y", (200 / 2));
+                        text.setAttribute("x", "50%");
+                        text.setAttribute("y", "50%");
+                        text.setAttribute("dominant-baseline", "middle");
+                        text.setAttribute("text-anchor", "middle");
                         text.style.fill = textColor;
                     }
                     el.setPercent = function(percent) {
@@ -64,23 +67,33 @@
                             }
                             elem.style.fill = color;
                             elem.innerHTML = text;
-                            var posInfo = elem.getBoundingClientRect();
-                            elem.setAttribute("x", (200 / 2) - (posInfo.width / 2));
-                            elem.setAttribute("y", (200 / 2));
+                            elem.setAttribute("x", "50%");
+                            elem.setAttribute("y", "50%");
+                            elem.setAttribute("dominant-baseline", "middle");
+                            elem.setAttribute("text-anchor", "middle");
                             return this;
                         } else {
                             console.error("text not set.");
                             return false;
                         }
                     }
+                    return el;
                 } else {
                     console.error("data-percent of ", el, "can't be greater than 100");
+                    return false;
                 }
             } else {
                 console.error("data-percent of ", el, "isn't a number");
+                return false;
             }
         } else {
-            console.error("data-percent of ", el, "is not set.")
+            console.error("data-percent of ", el, "is not set.");
+            return false;
         }
+    }
+}
+!(function() {
+    for(let el of document.querySelectorAll("div.circularProgress")) {
+        circularProgress.new(el);
     }
 })()
